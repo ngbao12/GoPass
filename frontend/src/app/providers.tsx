4448fceec35983f6234c58b40eb6991873458166
ngbao12 <ngocbao6422@gmail.com>
2026-01-07 23:53:56 +0700

    feat: update providers.tsx

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -1,8 +1,14 @@
 "use client";
 
 import { AuthProvider } from "@/features/auth/context/AuthContext";
+import { Toaster } from "sonner";
 import { ReactNode } from "react";
 
 export default function Providers({ children }: { children: ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>;
+  return (
+    <AuthProvider>
+      {children}
+      <Toaster position="top-right" richColors />
+    </AuthProvider>
+  );
 }
