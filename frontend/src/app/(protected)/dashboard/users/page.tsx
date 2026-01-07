8e6c621877790c46dc4c5199940658a0034fcdf7
ngbao12 <ngocbao6422@gmail.com>
2026-01-07 17:09:43 +0700

    docs: add docs for User Management of admin

---
+++ frontend/src/app/(protected)/dashboard/users/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from "react";
+import UserManagementView from "@/features/dashboard/components/admin/UserManagementView";
+
+export default function UsersPage() {
+  return <UserManagementView />;
+}
