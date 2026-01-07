707114da1b70ca7240e1f5e448d6d09ca5cec917
ngbao12 <ngocbao6422@gmail.com>
2026-01-08 00:40:20 +0700

    Revert "fix: update some files"
    This reverts commit 5a6ecb220c96b330d4a5f02068350c482404de2c.

---
+++ frontend/src/app/(protected)/dashboard/profile/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from "react";
+import ProfileView from "@/features/dashboard/components/profile/ProfileView";
+
+const ProfilePage: React.FC = () => {
+  return <ProfileView />;
+};
+
+export default ProfilePage;
